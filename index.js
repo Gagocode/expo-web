0fb369c338e82cdea7434b6ebf694f4e1ae68ed1
Gagocode <96364834+Gagocode@users.noreply.github.com>
2023-06-01 21:12:42 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var i = 0;
+var txt = 'Based on your parents problem it seems your a re the problem \n why dont u just kill yourself nobody loved you in the first place asshole'
+var speed = 50;
+
+async function show() {
+  const btn = document.getElementById("btn");
+  btn.remove();
+}
